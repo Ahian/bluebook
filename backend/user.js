2bff2a9a9e8f1ea389ad7193e81bbe880b088e81
AlmondLee <754125579@qq.com>
2019-08-29 20:57:50 +0800

    s

--- backend/user.js
+++ backend/user.js
@@ -1,7 +1,11 @@
 var superToken = getUrlParam('super_token');
 
-
-
+function getData () {
+    $.get("www.newbrandser.com/api/ti_xian_backend",function(data,status){
+        // JSON.parse(data)
+        console.log(data)
+    });
+}
 
 
 
