adf138c3acf6d5e9f859e31c65004f6505580009
Ahian <190030514@qq.com>
2019-09-11 18:45:34 +0800

    id

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a1d43c05d33788d2e5b046c93e28664e.js"
+  "/precache-manifest.3bdf2cd6da5633329d222b67d3ff473f.js"
 );
 
 workbox.clientsClaim();
