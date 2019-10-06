edfbc67a58c7d2d4ca1cd3073769ddbf91753f85
AlmondLee <754125579@qq.com>
2019-10-06 21:58:27 +0800

    s

---
+++ generate_user_json_stringlify.php
@@ -0,0 +1,9 @@
+<?php
+$user = (object)[
+    0 => "CC:8D:64:A0:F8:11",       //tys's watch
+    1 => "2C:FF:EE:DD:AB:2A",       //xzj's bluetooth
+    2 => "D0:FF:98:73:4A:40",       //lzh's bluetooth
+    1 => "2C:FF:EE:DD:AB:2B",       //xzj's wlan
+];
+
+echo json_encode($user);
