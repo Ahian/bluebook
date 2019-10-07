b92cf510ae27d5d723ba39d9357381872b05596c
Ahian <190030514@qq.com>
2019-10-07 12:35:10 +0800

    1: 172800,      //2*24*60*60      快递代取                     2: 120,//          电费代充                     3: 604800,//7*24*60*60   电影票代买

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a0729e408ae9a5c2cbcc26b2538a70f.js"
+  "/precache-manifest.e42399eced4a725c93d7fcbdb1b04ec2.js"
 );
 
 workbox.clientsClaim();
